ff71c785cc66ef53258c06d43c91e47df7a611a8
JFyrsten <jenny.fyrsten@agero.se>
2021-11-12 13:49:23 +0100

    - Next two tests

--- R2JPGomoku/R2JPGomokuLib/Board.cs
+++ R2JPGomoku/R2JPGomokuLib/Board.cs
@@ -12,7 +12,18 @@ namespace R2JPGomokuLib {
             public int Y { get; set; }
         }
 
+        
+
         public Board(List<List<string>> board) {
+            
+            foreach(var row in board)
+            {
+                for (int i = 0; i < row.Count; i++)
+                {
+                    if (row[i] == null)
+                        row[i] = "-";
+                }
+            }
             this.board = board;
         }
 
@@ -21,7 +32,37 @@ namespace R2JPGomokuLib {
         }
 
         public Move NextMove() {
+            var rowsAsStrings = Rows().Select(r => String.Join(string.Empty, r)).ToList();
+            var y = 0;
+            var x = 0;
+            for (int i = 0; i < rowsAsStrings.Count(); i++)
+            {
+                x = xOfFoundNextMove(rowsAsStrings[i], "xxxx-");
+                if (x != -1)
+                {
+                    y = i;
+                    return new Move { X = x, Y = y };
+                }
+                x = xOfFoundNextMove(rowsAsStrings[i], "-xxxx");
+                if (x != -1)
+                {
+                    y = i;
+                    return new Move { X = x, Y = y };
+                }
+            }
+
             return new Move() { X =  Rows().First().Count / 2,  Y = Rows().Count / 2 } ;
+        }
+
+        private int xOfFoundNextMove(string row, string pattern)
+        {
+            if (row.Contains(pattern))
+            {
+                var offset = pattern.IndexOf("-");
+                return row.IndexOf(pattern) + offset;
+
+            }
+            return -1;
         }
 
     }
--- R2JPGomoku/R2JPGomokuLibTests/BoardTests.cs
+++ R2JPGomoku/R2JPGomokuLibTests/BoardTests.cs
@@ -9,7 +9,7 @@ namespace R2JPGomokuLib.Tests {
     [TestClass()]
     public class BoardTests {
         [TestMethod()]
-        public void NextMoveTest() {
+        public void FirstTest_FirstMove() {
             var listBoard = new List<List<string>>() {
                 new List<string>() { null, null, null, null, null, null },
                 new List<string>() { null, null, null, null, null, null },
@@ -26,10 +26,48 @@ namespace R2JPGomokuLib.Tests {
             PerformTest(board, expected);
         }
 
+        [TestMethod()]
+        public void CanWeWin_FourInRow_Left()
+        {
+            var listBoard = new List<List<string>>() {
+                new List<string>() { null, null, null, null, null, null },
+                new List<string>() { null, null, null, null, null, null },
+                new List<string>() { "x", "x", "x", "x", null, null },
+                new List<string>() { null, null, null, null, null, null },
+                new List<string>() { null, null, null, null, null, null },
+                new List<string>() { null, null, null, null, null, null }
+            };
+
+            var board = new Board(listBoard);
+
+            var expected = new Move() { X = 4, Y = 2 };
+
+            PerformTest(board, expected);
+        }
+
+        [TestMethod()]
+        public void CanWeWin_FourInRow_Right()
+        {
+            var listBoard = new List<List<string>>() {
+                new List<string>() { null, null, null, null, null, null },
+                new List<string>() { null, null, null, null, null, null },
+                new List<string>() { null, null, "x", "x", "x", "x" },
+                new List<string>() { null, null, null, null, null, null },
+                new List<string>() { null, null, null, null, null, null },
+                new List<string>() { null, null, null, null, null, null }
+            };
+
+            var board = new Board(listBoard);
+
+            var expected = new Move() { X = 1, Y = 2 };
+
+            PerformTest(board, expected);
+        }
+
         private void PerformTest(Board board, Move expected) {
             var actual = board.NextMove();
-            Assert.AreEqual(expected.X, actual.X);
-            Assert.AreEqual(expected.Y, actual.Y);
+            Assert.AreEqual(expected.X, actual.X, $"Expected: X {expected.X} Actual: {actual.X}");
+            Assert.AreEqual(expected.Y, actual.Y, $"Expected: Y {expected.Y} Actual: {actual.Y}");
         }
     }
 }
