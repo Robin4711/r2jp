49e66438a66188d968fac127b91ef3a33df559b1
Robin Nordgren <robin.nordgren@agero.se>
2021-11-24 11:49:25 +0100

    Implemented diagonal tests

--- R2JPGomoku/R2JPGomokuLibTests/BoardDiagonalTests.cs
+++ R2JPGomoku/R2JPGomokuLibTests/BoardDiagonalTests.cs
@@ -10,38 +10,33 @@ namespace R2JPGomokuLib.Tests {
     public class BoardDiagonalTests {
         [TestMethod()]
         public void DiagonalsTest() {
+            string nul = null;
+
             var listBoard = new List<List<string>>() {
-                new List<string>() { "1A", "2A", "3A", "4A" },
-                new List<string>() { "1B", "2B", "3B", "4B" },
-                new List<string>() { "1C", "2C", "3C", "4C" },
-                new List<string>() { "1D", "2D", "3D", "4D" }
+                new List<string>() { nul, "x", "o", "x" },
+                new List<string>() { nul, "x", "o", "x" },
+                new List<string>() { nul, "x", "o", "x" },
+                new List<string>() { nul, "x", "o", "x" },
             };
 
             var board = new Board(listBoard, "x");
 
             var diagonals = board.Diagonals();
-            var actual = diagonals.Select(d => d.OrderBy(c => c.X).ThenBy(c => c.Y).ToList().ToStringExt()).ToList();
+            var actual = diagonals.Select(d => d.OrderBy(c => c.X).ToList().ToStringExt()).ToList();
 
             var expected = new List<string> {
-                "1D",
-                "1C, 2D", 
-                "1B, 2C, 3D",
-                "1A, 2B, 3C, 4D",
-                "2A, 3B, 4C",
-                "3A, 4B",
-                "4A",
-                "1A",
-                "1B, 2A",
-                "1C, 2B, 3A",
-                "1D, 2C, 3B, 4A",
-                "2D, 3C, 4B",
-                "3D, 4C",
-                "4D"
+                "-",
+                "-m",
+                "-mp",
+                "-mpm",
+                "mpm",
+                "pm",
+                "m",
             };
 
-            Assert.AreEqual(expected.Count, actual.Count);
+            Assert.AreEqual(expected.Count * 2, actual.Count);
             foreach (var exp in expected) {
-                Assert.IsTrue(actual.Contains(exp));
+                Assert.AreEqual(actual.Where(a => a.Equals(exp)).Count(), 2);
             }
         }
     }
---
+++ R2JPGomoku/R2JPGomokuLibTests/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static R2JPGomokuLib.Board;
+
+namespace R2JPGomokuLibTests {
+    public static class ExtensionMethods {
+        public static string ToStringExt(this IList<Move> self) {
+            return String.Join(", ", self.Select(m => $"(X:{m.X}, Y:{m.Y})"));
+        }
+    }
+}
